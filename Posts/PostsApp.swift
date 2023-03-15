bbee9b5ec14ba0eb67f21c8d9933a17168c8be09
haytham odeh <haythamlion@outlook.com>
2023-03-15 16:31:31 -0700

    Initial Commit

---
+++ Posts/PostsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostsApp.swift
+//  Posts
+//
+//  Created by Haytham Odeh on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct PostsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
